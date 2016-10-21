f0e90b5f7d0fdc2081985c6d63ec6a9fac7c402b
Daniel Imhoff <dwieeb@gmail.com>
2016-10-21 01:03:54 -0500

    add comment about expected result

--- index.ts
+++ index.ts
@@ -10,6 +10,6 @@ export function param() {
   return function(target: Object, propertyKey: string | symbol, parameterIndex: number) {
     console.log('PARAM: enter');
     const paramTypes = Reflect.getMetadata("design:paramtypes", target, propertyKey);
-    console.log('PARAM: paramTypes:', paramTypes);
+    console.log('PARAM: paramTypes:', paramTypes); // paramTypes is not supposed to be undefined
   };
 }
